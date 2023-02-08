3920c3e25f08bb7cbc46492af6ff22f5bb2b1479
Mert can Ünek <mrtcnunk@gmail.com>
2023-02-08 22:29:54 +0300

    .

--- BirthdayNoteTaker/ViewController.swift
+++ BirthdayNoteTaker/ViewController.swift
@@ -17,11 +17,36 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        
+        // We're attempting to retrieve the user default information that was saved when the application was launched.
+        
+        let storedName = UserDefaults.standard.object(forKey: "name")
+        let storedBirthday = UserDefaults.standard.object(forKey: "birthday")
+        
+        
+        // Casting - as? vs as!
+        if let newName = storedName as? String {
+            
+            nameLabel.text = newName
+        }
+        
+        if let newBirthday = storedBirthday as? String{
+            
+            birthdayLabel.text = newBirthday
+        }
+        
     }
 
 
     @IBAction func saveClicked(_ sender: Any) {
+        
+        
+        // save data
+        UserDefaults.standard.set(nameTextField.text!, forKey: "name")
+        UserDefaults.standard.set(birthdayTextField.text!, forKey: "birthday")
+        
+        
         
         nameLabel.text = "Name: \(nameTextField.text!)"
         birthdayLabel.text = "Birthday: \(birthdayTextField.text!)"
