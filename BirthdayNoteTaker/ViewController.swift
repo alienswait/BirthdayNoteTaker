a521efd374d0c3d4e96f2e77a66b3655684d14f6
Mert can Ünek <mrtcnunk@gmail.com>
2023-02-08 21:31:38 +0300

    Initial Commit

---
+++ BirthdayNoteTaker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BirthdayNoteTaker
+//
+//  Created by Mertcan Ünek on 8.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
